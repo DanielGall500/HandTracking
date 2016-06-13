a8b58cb7fda5d8b8aabcaa8bf028ca79e905ec9a
DannyGotTea <danielgallagher500@gmail.com>
2016-06-13 11:53:09 +0100

    Initial Commit

---
+++ HandTracking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HandTracking
+//
+//  Created by Daniel Gallagher on 13/06/2016.
+//  Copyright (c) 2016 Developer Co. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

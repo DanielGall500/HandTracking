4402d52a702b3f9210434055f65e5259ee6662df
DannyGotTea <danielgallagher500@gmail.com>
2016-06-13 13:18:33 +0100

    MOG2 background subtraction

--- HandTracking/main.cpp
+++ HandTracking/main.cpp
@@ -6,11 +6,19 @@
 //  Copyright (c) 2016 Developer Co. All rights reserved.
 //
 
-#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <sstream>
 
 using namespace cv;
 using namespace std;
+
+Mat frame, edges;
+
+Mat foregroundMask;
+
+Ptr<BackgroundSubtractor> pMOG2;
+int keyboard;
 
 int main(int argc, const char * argv[]) {
     VideoCapture cap(0);
@@ -18,17 +26,30 @@ int main(int argc, const char * argv[]) {
     if(!cap.isOpened())
         return -1;
     
+    pMOG2 = createBackgroundSubtractorMOG2();
+    
     while(true)
     {
-        Mat frame, edges;
         
         cap >> frame;
-        
+    
         cvtColor(frame, frame, CV_BGR2GRAY);
-        GaussianBlur(frame, frame, Size(7,7), 1.5);
-        Canny(frame, edges, 0, 30);
         
-        imshow("Edges", edges);
+        pMOG2->apply(frame, foregroundMask);
+        
+        stringstream ss;
+        
+        rectangle(frame, Point(10, 2), Point(100,20), Scalar(255,255,255), -1);
+        
+        ss << cap.get(CAP_PROP_POS_FRAMES);
+        
+        string frameNumberString = ss.str();
+        
+        putText(frame, frameNumberString.c_str(), Point(15, 15),
+                FONT_HERSHEY_COMPLEX, 0.5, Scalar(0,0,0));
+        
+        imshow("Frame", frame);
+        imshow("Mask", foregroundMask);
         
         if(waitKey(30) >= 0)
             break;
@@ -36,3 +57,7 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+
+
+

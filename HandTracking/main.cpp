e70fb8ef88eab82133ea521e5db385ce108acc3a
DannyGotTea <danielgallagher500@gmail.com>
2016-06-24 14:56:58 +0100

    Resources added + frame counter implemented

--- HandTracking/main.cpp
+++ HandTracking/main.cpp
@@ -6,9 +6,14 @@
 //  Copyright (c) 2016 Developer Co. All rights reserved.
 //
 
-#include <opencv2/opencv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+
 #include <iostream>
+#include <string.h>
 #include <sstream>
+#include <fstream>
 
 using namespace cv;
 using namespace std;
@@ -16,39 +21,101 @@ using namespace std;
 int BskinMin = 130, GskinMin = 160, RskinMin = 230;
 int BskinMax = 200, GskinMax = 220, RskinMax = 255;
 
-bool inRange(int val, int min, int max);
-Mat findSkin(Mat frame);
+string file_loc = "/Users/dannyg/Desktop/Projects/HandTracking/HandTracking/Resources/";
+string face_front = "/Users/dannyg/Desktop/haarcascade_frontalface_alt.xml";
+string skin_loc = "/Users/dannyg/Desktop/Projects/Hand\ Tracking/HandTracking/Resources/SkinColour.jpg";
+
+CascadeClassifier faceCascade;
+vector<Rect> faces;
+Mat skinColour;
+
+bool withinRange(int val, int min, int max);
+
+Mat findPrimaryColour(Mat frame);
+Vec3b captureSkinColour(Mat frame);
+
+void detectFaces(Mat frame, vector<Rect> &outputFaces);
+
+void read_images(std::string imgs_filename, vector<Mat> &images, vector<int> labels, char seperator = ';')
+{
+    std::stringstream fileStream(imgs_filename.c_str(), ifstream::in);
+    
+    if(!fileStream)
+        CV_Error(CV_StsBadArg, "Invalid Filestream Input");
+    
+    string line, path, classLabel;
+    
+    while (getline(fileStream, line))
+    {
+        stringstream lineSS(line);
+        
+        getline(lineSS, path, seperator);
+        
+        getline(lineSS, classLabel);
+        
+        if(!path.empty() && !classLabel.empty())
+        {
+            images.push_back(imread(path, 0));
+            labels.push_back(atoi(classLabel.c_str()));
+        }
+    }
+    
+}
 
 int main(int argc, const char * argv[]) {
     
     VideoCapture cap(0); //always BGR
+    uint frameCounter = -1;
     
     if(!cap.isOpened())
         return -1;
     
-    namedWindow("Testing");
+    faceCascade.load(face_front);
     
-    createTrackbar("BlueMin", "Testing", &BskinMin, 255);
-    createTrackbar("GreenMin", "Testing", &GskinMin, 255);
-    createTrackbar("RedMin", "Testing", &RskinMin, 255);
+    skinColour = imread(skin_loc);
+    cvtColor(skinColour, skinColour, CV_RGB2BGR);
     
-    createTrackbar("BlueMax", "Testing", &BskinMax, 255);
-    createTrackbar("GreenMax", "Testing", &GskinMax, 255);
-    createTrackbar("RedMax", "Testing", &RskinMax, 255);
-    
-    while(true)
+    if(!faceCascade.load(face_front))
     {
-        Mat frame, blurred;
+        cout << "Error loading cascade" << endl;
+    }
+    
+    while (true)
+    {
+        Mat frame;
         
         cap >> frame;
+        frameCounter++;
         
-        GaussianBlur(frame, blurred, Size(7,7), 1.5);
+        if (frameCounter <= 24)
+        {
+            detectFaces(frame, faces);
+            
+            for (int i = 0; i < faces.size(); i++)
+            {
+                Point center(faces[i].x + faces[i].width * 0.5, faces[i].y + faces[i].height * 0.5);
+                
+                ellipse(frame, center, Size(faces[i].width * 0.5, faces[i].height * 0.5),
+                        0, 0, 360, Scalar(0,255,0), 4, 8, 0);
+            }
+        }
         
-        Mat newFrame = findSkin(blurred);
+        if(!frame.empty())
+        {
+            
+        }
+        else
+        {
+            cout << "Empty frame: " << endl;
+        }
         
-        cout << newFrame.at<Vec3b>(0,0) << endl;
+        //GaussianBlur(frame, blurred, Size(7,7), 1.5);
         
-        imshow("Testing", newFrame);
+        //Mat newFrame = findSkin(blurred);
+        
+        //cout << newFrame.at<Vec3b>(0,0) << endl;
+        
+        imshow("Testing", frame);
         
         if(waitKey(30) > 0)
             return -1;
@@ -58,12 +125,20 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+void detectFaces(Mat frame, vector<Rect> &outputFaces)
+{
+    cvtColor(frame, frame, CV_BGR2GRAY);
+    equalizeHist(frame, frame);
+    
+    faceCascade.detectMultiScale(frame, outputFaces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30,30));
+}
+
 bool withinRange(int val, int min, int max) //efficiently checks whether val is in between min and max
 {
     return (unsigned)(val - min) <= (max - min);
 }
 
-Mat findSkin(Mat frame)
+Mat findPrimaryColour(Mat frame)
 {
     Vec3b colChannels;
     int b, g, r;
@@ -78,20 +153,7 @@ Mat findSkin(Mat frame)
             g = colChannels[1];
             r = colChannels[2];
             
-            /*if(withinRange(b, BskinMin, BskinMax) &&
-               withinRange(g, GskinMin, GskinMax) &&
-               withinRange(r, RskinMin, RskinMax))*/
             
-            if(withinRange(b, BskinMin, BskinMax) &&
-               withinRange(g, GskinMin, GskinMax) &&
-               withinRange(r, RskinMin, RskinMax))
-            {
-                colChannels = Vec3b(255,255,255);
-            }
-            else
-            {
-                colChannels = Vec3b(0,0,0);
-            }
             
             frame.at<Vec3b>(rows, cols) = colChannels;
             
@@ -99,15 +161,6 @@ Mat findSkin(Mat frame)
     }
     return frame;
 }
-
-
-
-
-
-
-
-
-
 
 
 
